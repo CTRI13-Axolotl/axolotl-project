cb16dea36c2bb6545a44b57ecc16c77855567228
BrandonMuniz1 <brandonmuniz1@gmail.com>
2022-12-31 13:10:51 -0500

    Profile update

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router";
-
+// import {handleSubmit} from './login';
 export default function Profile (){
+
+
     return(
         <div>
         <h1>Profile</h1>
@@ -8,7 +10,26 @@ export default function Profile (){
 
     </div>
     <div className="stats">
+     <div className="eating">
+        <div className="stat-name">Eat</div>
+       <div className="stat-bar">
+        <div className="stat-per" per="20%" id="eatStat"></div>
+        </div> 
+     </div>
 
+     <div className="pooping">
+        <div className="stat-name">Poop</div>
+        <div className="stat-bar">
+            <div className="stat-per" per="35%" id="poopStat"></div>
+        </div>
+     </div>
+
+     <div className="playing">
+        <div className="stat-name">Play</div>
+        <div className="stat-bar">
+            <div className="stat-per" per='50%'id="playStat" ></div>
+        </div>
+     </div>
     </div>
     <div className="imageRow" >
      <img id="picOne" src="https://i.pinimg.com/originals/75/c8/cc/75c8cca0f911f1a9c4a2326291131a2f.png" alt="eat" />
