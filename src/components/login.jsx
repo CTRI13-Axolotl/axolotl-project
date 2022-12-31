4509c497b8acadba3dd63a4d1ae875abd0f5b27a
Chris Tenorio <christenorio@Chriss-MBP.fios-router.home>
2022-12-31 13:46:49 -0800

    login page img & div added from failed pull

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-    import react from 'react'
+    import react from 'react';
     import LoginButton from './Glogin';
     import LogoutButton from './Glogout';
     import {useEffect} from 'react';
@@ -60,14 +60,20 @@ class Login extends react.Component{
         return(
             <div>
                 {/* //place our logo or main thing here */}
+                <img id='photo'src="https://static.vecteezy.com/system/resources/previews/006/372/807/original/cute-axolotl-cartoon-mascot-illustration-eating-noodle-vector.jpg" alt="ok" />
+            <div className='container'>
+                <h1 id='loghead'>Login</h1>
                  <form onSubmit ={this.handleSubmit}>
                   <input className='email' type='email' name='email' placeholder='email...' required onChange={this.handleChange} />
                   <input className='password' type='password' name='password' placeholder='password...' required onChange={this.handleChange} />
+                  <div id="buttonRow">
                   <button className='login' onSubmit={this.handleSubmit}> Log in</button>
                   <button className='register' onSubmit={this.handleSubmit}> Register </button>
+                  </div>
                   <LoginButton />
                   <LogoutButton />
                 </form>
+            </div>
             </div>
         )
     }
